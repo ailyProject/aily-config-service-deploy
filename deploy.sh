8908b79dc945ba57ac292b819af2847ac970196a
stao <werewolf_st@hotmail.com>
2024-04-30 20:40:59 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-sudo apt-get update && sudo apt install build-essential libssl-dev zlib1g-dev \
+# 更新系统
+sudo apt-get update && sudo apt-get upgrade -y
+
+# 安装依赖
+sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev supervisor -y
 
